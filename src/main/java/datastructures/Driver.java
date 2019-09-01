e18a2da693b5df0d8835cc54ccc393985fc84a58
William Broach <william.broach@gmail.com>
2019-09-01 14:27:23 -0400

    initial commit of build

---
+++ src/main/java/datastructures/Driver.java
@@ -0,0 +1,13 @@
+package datastructures;
+
+import datastructures.utils.BinaryTreeUtils; 
+
+public class Driver {
+
+
+    public static void main(String[] args) {
+
+	BinaryTreeUtils.printTree();
+    }
+
+}
---
+++ src/main/java/datastructures/utils/BinaryTreeUtils.java
@@ -0,0 +1,12 @@
+package datastructures.utils; 
+
+public class BinaryTreeUtils {
+
+    public static void printTree() {
+
+	System.out.println("TEST SUCCEEDED!!");
+
+    }
+
+
+}
